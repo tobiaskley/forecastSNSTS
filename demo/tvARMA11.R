ca8d92da72533e63e8abb5aaf09185baf379c2dc
tobiaskley <tobias.kley@ruhr-uni-bochum.de>
2016-11-11 16:36:07 +0000

    Replaced the old LondonHPI demo with the new tvARMA11 demo.

--- demo/tvARMA11.R
+++ demo/tvARMA11.R
@@ -1,39 +1,48 @@
 
-## compute monthly changes of average prices
-L <- length(LondonHPI)
-LondonHPI_change <- LondonHPI[2:L]/LondonHPI[1:(L-1)] - 1
+## In this demo we demonstrate how the data analysis in Section 5 of
+## Kley et al (2016) can be performed, but with artificial data.
 
-## demean the data
-LondonHPI_mean <- mean(LondonHPI_change)
-LondonHPI_adj <- ts(LondonHPI_change - LondonHPI_mean, f = 12, start=c(1995,2))
+## To this end, we generate artificial data for the example analysis.
+## We use a tvARMA(1,1) process with the following coefficient functions:
+a     <- list( function(u) { return(u - 0.5) } )
+b     <- list( function(u) { return( 1 + sin(pi * u) ) } )
+sigma <- function(u) { return( 1 + 3 * u ) }
 
-L <- length(LondonHPI_adj)
+## The function tvARMA from this package can then be used to simulate
+X <- tvARMA(T = 250, a, b, sigma)
 
-p_max <- 18
+## Show what the path looks like.
+plot(X, type="l")
+
+## We now choose the maximum model order p_max and the size of the
+## validation and end of the training set m. 
+p_max <- 7
+m <- 20
+
+## We also choose the maximum forecasting horizon
 H <- 6
 
-m <- 12
+## Given the length L of the time series we simulated
+L <- length(X)
 
-m0 <- L - 3*m # 215
-m1 <- L - 2*m # 227
-m2 <- L - m   # 239
-m3 <- L       # 251
-
-## Show which observations are in the final part of the training set,
-## the validation set and the test set, respectively.
-
-LondonHPI_adj[(m0+1):m1]
-LondonHPI_adj[(m1+1):m2]
-LondonHPI_adj[(m2+1):m3]
+## We have
+## the end point m0 of the main part of the training set,
+## the end point m1 of the final part of the training set,
+## the end point m2 of the validation set and
+## the end point m3 of the test set: 
+m0 <- L - 3*m # 190
+m1 <- L - 2*m # 210
+m2 <- L - m   # 230
+m3 <- L       # 250
 
 ## define the set \mathcal{N}; i.e., which N to compute the predictions for
 Ns <- c(0,seq(min(p_max+1, ceiling((L/2)^(4/5))), ceiling(L^(4/5))))
 
 ## compute all the linear prediction coefficients needed
-coef0 <- predCoef(LondonHPI_adj, p_max, H, (m0-H+1):(m3-1), Ns)
+coef0 <- predCoef(X, p_max, H, (m0-H+1):(m3-1), Ns)
 
 ## compute the MSPE on the final part of the training set
-mspe <- MSPE(LondonHPI_adj, coef0, m0+1, m1, p_max, H, Ns)
+mspe <- MSPE(X, coef0, m0+1, m1, p_max, H, Ns)
 
 ## compute the minima for all N >= N_min
 N_min <- 35
@@ -42,28 +51,30 @@ N <- mspe$N
 
 res_e <- matrix(0, nrow = H, ncol = 5)
 for (h in 1:H) {
-  res_e[h, 1] <- idx1_s <- which(M[h, , N == 0] == min(M[h, , N == 0]), arr.ind = TRUE)[1]
+  res_e[h, 1] <- idx1_s <- which(M[h, , N == 0] == min(M[h, , N == 0]),
+                                 arr.ind = TRUE)[1]
   res_e[h, 2] <- min(M[h, , N == 0])
-  idx1_ls <- which(M[h, , N != 0 & N >= N_min] == min(M[h, , N != 0 & N >= N_min]), arr.ind = TRUE)[1,]
+  idx1_ls <- which(M[h, , N != 0 & N >= N_min] == min(M[h, , N != 0 & N >= N_min]),
+                   arr.ind = TRUE)[1,]
   res_e[h, 3] <- idx1_ls[1]
   res_e[h, 4] <- N[N != 0 & N >= N_min][idx1_ls[2]]
   res_e[h, 5] <- min(M[h, , N != 0 & N >= N_min])
 }
 
-## Top rows from Table 5 in Kley et al (2016)
+## A table similar to the top row from Table 5 in Kley et al (2016):
 res_e
 
 ## compute the MSPE of the null predictor
-vr <- sum(LondonHPI_adj[(m0 + 1):m1]^2) / (m1 - m0)
+vr <- sum(X[(m0 + 1):m1]^2) / (m1 - m0)
 
-## Top plot from Figure 4 in Kley et al (2016)
+## A plot similar to the top plot from Figure 4 in Kley et al (2016)
 plot(mspe, vr = vr, N_min = N_min, h = 1, add.for.legend=15)
 
-## Bottom plot from Figure 4 in Kley et al (2016)
+## A plot similar to the bottom plot from Figure 4 in Kley et al (2016)
 plot(mspe, vr = vr, N_min = N_min, h = 6, add.for.legend=15)
 
 ## compute MSPE on the validation set 
-mspe <- MSPE(LondonHPI_adj, coef0, m1 + 1, m2, p_max, H, Ns)
+mspe <- MSPE(X, coef0, m1 + 1, m2, p_max, H, Ns)
 M <- mspe$mspe
 N <- mspe$N
 
@@ -79,7 +90,7 @@ for (h in 1:H) {
 }
 
 ## compute MSPE on the validation set 
-mspe <- MSPE(LondonHPI_adj, coef0, m2 + 1, m3, p_max, H, Ns)
+mspe <- MSPE(X, coef0, m2 + 1, m3, p_max, H, Ns)
 M <- mspe$mspe
 N <- mspe$N
 
@@ -94,5 +105,5 @@ for (h in 1:H) {
   res_t[h, 3] <- res_t[h, 1] / res_t[h, 2]
 }
 
-## Bottom rows from Table 5 in Kley et al (2016)
+## A table similar to the bottom rows from Table 5 in Kley et al (2016)
 cbind(res_v, res_t)
