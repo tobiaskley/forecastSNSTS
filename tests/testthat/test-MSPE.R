105c161708c0c468bac092efa8347355728411b9
tobiaskley <tobias.kley@ruhr-uni-bochum.de>
2016-11-15 16:31:48 +0000

    Added a test to check parameter validation of MSPE.

--- tests/testthat/test-MSPE.R
+++ tests/testthat/test-MSPE.R
@@ -32,19 +32,53 @@ for (h in 1:H) {
   }
 }
 
-predcoef <- predCoef(X, P, H, (m1-H):(m2-1), N )
-cppRes <- MSPE(X, predcoef, m1, m2, P, H, N)
-
-#library(microbenchmark)
-#microbenchmark(
-#    {cppRes <- MSPE(X, coef, m1, m2, p, h, N)},
-#    {cppRes <- MSPE(X, m1=m1, m2=m2, P=p, H=h, N=N)}
-#  )
-
-#naiveRes[2, , ]
-#cppRes$mspe[2, , ]
 
 
 test_that("mspe computation works", {
+  predcoef <- predCoef(X, P, H, (m1-H):(m2-1), N )
+  cppRes <- MSPE(X, predcoef, m1, m2, P, H, N)
   expect_equal(sum( (cppRes$mspe - naiveRes)^2 ), 0)
+  
+  # It should also work without the predcoef previously computed.
+  cppRes1 <- MSPE(X, m1 = m1, m2 = m2, P = P, H = H, N = N)
+  expect_equal(sum( (cppRes1$mspe - naiveRes)^2 ), 0)
+})
+
+test_that("parameter validation works properly", {
+  expect_error( cppRes <- MSPE(X, "not a list", m1, m2, P, H, N) )
+  
+  predcoef_not_right_names <- list()
+  expect_error( cppRes <- MSPE(X, predcoef_not_right_names, m1, m2, P, H, N) )
+  
+  predcoef_not_right_names <- list(coef_X = array(dim = c(P,P,H,m2-m1+1,length(N))), t = (m1+1):m2, N = N)
+  expect_error( cppRes <- MSPE(X, predcoef_not_right_names, m1, m2, P, H, N) )
+
+  ## predcoef$pref has to be five dimensions
+  predcoef_not_right_dim1 <- list(coef = array(dim = c(1,P,P,H,m2-m1+1,length(N))), t = (m1+1):m2, N = N)
+  expect_error( cppRes <- MSPE(X, predcoef_not_right_dim1, m1, m2, P, H, N) )
+  
+  predcoef_t_not_numeric <- list(coef = array(dim=c(1,2,3,4,5)), t = "not numeric", N = 3:7)
+  expect_error( cppRes <- MSPE(X, predcoef_t_not_numeric, m1, m2, P, H, N) )
+  
+  predcoef_N_not_numeric <- list(coef = array(dim=c(1,2,3,4,5)), t = 5:10, N = "not numeric")
+  expect_error( cppRes <- MSPE(X, predcoef_N_not_numeric, m1, m2, P, H, N) )
+  
+  predcoef_not_right_dim2 <- list(coef = array(dim = c(P,P+1,H,m2-m1+1,length(N))), t = (m1+1):m2, N = N)
+  expect_error( cppRes <- MSPE(X, predcoef_not_right_dim2, m1, m2, P, H, N) )
+  
+  predcoef_not_right_dim3 <- list(coef = array(dim = c(P-1,P-1,H,m2-m1+1,length(N))), t = (m1+1):m2, N = N)
+  expect_error( cppRes <- MSPE(X, predcoef_not_right_dim3, m1, m2, P, H, N) )
+  
+  predcoef_not_right_dim4 <- list(coef = array(dim = c(P,P,H-1,m2-m1+1,length(N))), t = (m1+1):m2, N = N)
+  expect_error( cppRes <- MSPE(X, predcoef_not_right_dim4, m1, m2, P, H, N) )
+  
+  predcoef_not_right_dim5 <- list(coef = array(dim = c(P,P,H,m2-m1+1,length(N))), t = (m1+1):(m2+1), N = N)
+  expect_error( cppRes <- MSPE(X, predcoef_not_right_dim5, m1, m2+1, P, H, N) )
+  
+  predcoef_too_few_N <- list(coef = array(dim = c(P,P,H,m2-m1+1,length(N))), t = (m1+1):m2, N = 10:11)
+  expect_error( cppRes <- MSPE(X, predcoef_too_few_N, m1, m2, P, H, 10:11) )
+
+  predcoef_too_few_t <- list(coef = array(dim = c(P,P,H,m2-m1+1,length(N))), t = m1:m2, N = 10:11)
+  expect_error( cppRes <- MSPE(X, predcoef_too_few_t, m1, m2, P, H, N) )
+  
 })
